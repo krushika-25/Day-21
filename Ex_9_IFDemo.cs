d81faf93a69751282923da55fe1a377b5e88c21d
Krushika <137705754+krushika-25@users.noreply.github.com>
2023-07-14 13:54:39 +0530

    Add files via upload

---
+++ Ex_9_IFDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Ex_9_IFDemo
+    {
+        static void Main(string[] args)
+        {
+            int time = 22;
+            if (time < 10)
+            {
+                Console.WriteLine("Good morning.");
+            }
+            else if (time < 20)
+            {
+                Console.WriteLine("Good day.");
+            }
+            else
+            {
+                Console.WriteLine("Good evening.");
+            }
+        }
+    }
+}
+//For Loop 
+
